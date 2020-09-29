d4175ffd5183715951ffb93d889309f2c4c51576
diablo9983 <diablo9983@gmail.com>
2020-09-30 01:08:12 +0200

    fix: Make WorkbenchItem component functional

--- src/components/shared/WorkbenchItem.vue
+++ src/components/shared/WorkbenchItem.vue
@@ -1,19 +1,9 @@
-<template>
-  <component :is="tag" class="layers-item">
-    <div class="layers-item-toolbox">
-
-    </div>
-    <slot />
-  </component>
-</template>
-
 <script>
-
 const validTypes = ['section', 'container', 'item']
 
 export default {
   name: 'WorkbenchItem',
-
+  functional: true,
   props: {
     tag: {
       type: String,
@@ -24,6 +14,21 @@ export default {
       validator: value => validTypes.includes(value),
       default: 'item'
     }
+  },
+  render (h, { data, props, scopedSlots }) {
+    const toolbox = h('div', {
+      staticClass: 'layers-item-toolbox'
+    })
+
+    const staticClass = ['layers-item', 'layers-item--' + props.type]
+    if (data.staticClass) {
+      staticClass.push(...data.staticClass.split(' '))
+    }
+
+    return h(props.tag, {
+      staticClass: staticClass.join(' '),
+      class: data.class
+    }, [toolbox, scopedSlots.default()])
   }
 }
 </script>
